6b8747a620edb9e8ff2b37bb079062a89708d829
Miguel Rodriguez <mrodriguez10100@gmail.com>
2021-09-28 21:21:17 -0500

    Initial Commit

---
+++ Upo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Upo
+//
+//  Created by Miguel Rodriguez on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Upo/UpoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpoApp.swift
+//  Upo
+//
+//  Created by Miguel Rodriguez on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct UpoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
